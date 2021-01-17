8be090efdde1aecd0a452e574b8763dc924f9443
Alvaro Alepuz <alvaroalepuz99@gmail.com>
2021-01-17 21:32:10 +0100

    Ae8 Acabada

---
+++ src/app/Interfaces.ts
@@ -0,0 +1,25 @@
+export interface IProducto{
+    "id":number,
+    "nombre":string,
+    "descripcion":string,
+    "precio":number,
+    "categoria":string,
+}
+
+export interface IMotor extends IProducto{
+    "tipo":string,
+    "km":number,
+    "anyo":number,
+}
+
+export interface IInmobiliaria extends IProducto{
+    "m2":number,
+    "nbanyos":number,
+    "nhabitaciones":number,
+    "localidad":string,
+
+}
+
+export interface ITecnologia extends IProducto{
+    "estado":string,
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import{IProducto} from  '../Interfaces'
-
+import { IProducto, IMotor, IInmobiliaria, ITecnologia } from '../Interfaces';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,38 +10,104 @@ export class HomePage {
 
   cat: string = "Categoria";
   tipo: string = "number";
-  categoria: string = 'categoria';
-
 
   nombre: string;
   descripcion: string;
-  precio: string;
+  precio: number;
+  categoria: string;
 
-  productos: IProducto[] = [
-    {
-      "id": 1,
-      "nombre": "casa",
-      "descripcion": "Casa a las afueras",
-      "precio": 500
-    },
-    {
-      "id": 2,
-      "nombre": "Ordenador",
-      "descripcion": "Casa a las afueras",
-      "precio": 500
+  tipos: string;
+  km: number;
+  anyo: number;
+
+  m2: number;
+  nbanyos: number;
+  nhabitaciones: number;
+  localidad: string;
+
+  estado: string;
+
+  productos: (IProducto | IInmobiliaria | IMotor | ITecnologia)[] = [];
+
+  constructor(public alertController: AlertController) { }
+
+  async alerta(): Promise<void> {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Atencion',
+      message: 'Se ha añadido un nuevo producto.',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
+
+  async insertar() {
+
+    switch (this.categoria) {
+      case "motor":
+        this.productos.push({
+          "id": this.productos.length + 1,
+          "nombre": this.nombre,
+          "descripcion": this.descripcion,
+          "precio": this.precio,
+          "categoria": this.categoria,
+          "tipo": this.tipo,
+          "km": this.km,
+          "anyo": this.anyo
+        })
+        this.alerta();
+        break;
+      case "inmobiliaria":
+        this.productos.push({
+          "id": this.productos.length + 1,
+          "nombre": this.nombre,
+          "descripcion": this.descripcion,
+          "precio": this.precio,
+          "categoria": this.categoria,
+          "m2": this.m2,
+          "nbanyos": this.nbanyos,
+          "nhabitaciones": this.nhabitaciones,
+          "localidad": this.localidad
+
+        })
+        this.alerta();
+        break;
+        case "tecnologia":
+          this.productos.push({
+            "id": this.productos.length + 1,
+            "nombre": this.nombre,
+            "descripcion": this.descripcion,
+            "precio": this.precio,
+            "categoria": this.categoria,
+            "estado": this.estado
+  
+          })
+          this.alerta();
+        break;
     }
-  
-  
-  
-  ];
-
-
-
-
-
-  constructor() { }
-
-
-
-
+  }
 }
+
+
+
+
+
+
+  //async insertar() {
+  //  this.productos.push({
+  //  "id": this.productos.length + 1,
+  //"nombre": this.nombre,
+  //  "descripcion": this.descripcion,
+  // "precio": this.precio,
+  // "categoria": this.categoria
+
+  //})
+  // const alert = await this.alertController.create({
+  //   cssClass: 'my-custom-class',
+  //   header: 'Atencion',
+  //   message: 'Se ha añadido un nuevo proucto.',
+  //   buttons: ['Aceptar']
+  // });
+
+  // await alert.present();
