697372554df2416320b11062ac10323ac1555033
Alvaro Alepuz <alvaroalepuz99@gmail.com>
2021-01-18 22:34:17 +0100

    Empezando ae9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -27,87 +27,27 @@ export class HomePage {
 
   estado: string;
 
-  productos: (IProducto | IInmobiliaria | IMotor | ITecnologia)[] = [];
+  productos: (IProducto | IInmobiliaria | IMotor | ITecnologia)[] = [
+    {
+      "id": 1,
+      "nombre": "Casa",
+      "descripcion": "casa con piscina",
+      "categoria":"tecnologia",
+      "precio":56,
+      "estado":"nuevo"
+
+    },
+     {
+      "id": 1,
+      "nombre": "Casa",
+      "descripcion": "casa con piscina",
+      "categoria":"Hogar",
+      "precio":56
+
+    }
+  ];
 
   constructor(public alertController: AlertController) { }
 
-  async alerta(): Promise<void> {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Atencion',
-      message: 'Se ha añadido un nuevo producto.',
-      buttons: ['Aceptar']
-    });
 
-    await alert.present();
-  }
-
-  async insertar() {
-
-    switch (this.categoria) {
-      case "motor":
-        this.productos.push({
-          "id": this.productos.length + 1,
-          "nombre": this.nombre,
-          "descripcion": this.descripcion,
-          "precio": this.precio,
-          "categoria": this.categoria,
-          "tipo": this.tipo,
-          "km": this.km,
-          "anyo": this.anyo
-        })
-        this.alerta();
-        break;
-      case "inmobiliaria":
-        this.productos.push({
-          "id": this.productos.length + 1,
-          "nombre": this.nombre,
-          "descripcion": this.descripcion,
-          "precio": this.precio,
-          "categoria": this.categoria,
-          "m2": this.m2,
-          "nbanyos": this.nbanyos,
-          "nhabitaciones": this.nhabitaciones,
-          "localidad": this.localidad
-
-        })
-        this.alerta();
-        break;
-        case "tecnologia":
-          this.productos.push({
-            "id": this.productos.length + 1,
-            "nombre": this.nombre,
-            "descripcion": this.descripcion,
-            "precio": this.precio,
-            "categoria": this.categoria,
-            "estado": this.estado
-  
-          })
-          this.alerta();
-        break;
-    }
-  }
 }
-
-
-
-
-
-
-  //async insertar() {
-  //  this.productos.push({
-  //  "id": this.productos.length + 1,
-  //"nombre": this.nombre,
-  //  "descripcion": this.descripcion,
-  // "precio": this.precio,
-  // "categoria": this.categoria
-
-  //})
-  // const alert = await this.alertController.create({
-  //   cssClass: 'my-custom-class',
-  //   header: 'Atencion',
-  //   message: 'Se ha añadido un nuevo proucto.',
-  //   buttons: ['Aceptar']
-  // });
-
-  // await alert.present();
